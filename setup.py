d23ec47ab18c5bfc5b93a93ea94330c27102b0d7
Tim Copeland <timothy.copeland@harbingermotors.com>
2025-04-07 09:51:08 -0700

    Updating setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,13 @@ from setuptools import setup
 setup(
     name='pytest-json',
     version='0.0.1',
+    author='Tim Copeland',
+    license='MIT',
+    description='A pytest plugin to generate JSON reports, with ATX support.',
+    url='https://github.com/tpc8486/test_guide_pytest_json',
     packages=['pytest_json'],
-    install_requires=['pytest', 'ruff'],
+    install_requires=['pytest'],
+    python_requires='>=3.10',
     entry_points={
         'pytest11': [
             'json = pytest_json.plugin',
