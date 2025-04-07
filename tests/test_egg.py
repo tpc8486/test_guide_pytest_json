e3a66979dc4b4f39ee5748d850497c80c79057d4
Tim Copeland <timothy.copeland@harbingermotors.com>
2025-04-07 12:05:39 -0700

    Adding more tests

--- tests/test_egg.py
+++ tests/test_egg.py
@@ -1,9 +1,18 @@
+import warnings
+
 import pytest
 
 
 def test_egg_hatch():
-    assert True
-
+    assert 2 + 2 == 5
 
 def test_egg_before_chicken():
     pytest.fail("This test is designed to fail")
+
+def test_exception():
+    with pytest.raises(ValueError):
+        raise ValueError("This is an expected error")
+
+@pytest.mark.filterwarnings("error")
+def test_warning():
+    warnings.warn("This is a warning")
